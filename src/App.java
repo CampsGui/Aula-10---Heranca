3687f1d5322a2231d422da68cea7aeab2a917f0a
Guilherme Alves <guigui21alves@gmail.com>
2024-11-13 11:58:34 -0300

    [feat] Classes Mensalista, Horista e App.Java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Mensalista m1 = new Mensalista("José", "Rua ABC Nº123", "Junior");
+        Horista h1 = new Horista("Maria", "Rua 23 de Maio", 10);
+
+        System.out.println("INSS: " + m1.calcularINSS());
+        System.out.println("IR: " + m1.calcularIR());
+
+
     }
 }
--- src/Horista.java
+++ src/Horista.java
@@ -1,3 +1,18 @@
-public class Horista{
+public class Horista extends Empregado{
+    private int horas;
+
+    public Horista(String nome, String endereco, int horas) {
+        super(nome, endereco);
+        this.horas = horas;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+    
     
 }
