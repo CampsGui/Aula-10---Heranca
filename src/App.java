d57b3c76421745758a201613d71a8ad44e125fc8
Guilherme Alves <guigui21alves@gmail.com>
2024-11-13 12:43:41 -0300

    [feat] seila

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Mensalista m1 = new Mensalista("José", "Rua ABC Nº123", "Junior");
+        Mensalista m1 = new Mensalista("José", "Rua ABC Nº123", "Pleno");
         Horista h1 = new Horista("Maria", "Rua 23 de Maio", 10);
         
         m1.calcularSalario();
@@ -9,7 +9,7 @@ public class App {
         System.out.println("INSS: " + m1.calcularINSS());
         System.out.printf("IR: %.2f%n", m1.calcularIR());
         System.err.println("\n");
-        
+
         h1.calcularSalario();
 
         System.out.println("HORISTA: " + h1.getNome());        
